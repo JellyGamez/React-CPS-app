b0e973696570e4693e9282bdb47c26938e3a0e90
JellyGamez <75379898+JellyGamez@users.noreply.github.com>
2023-08-12 14:31:57 +0300

    no more flickering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
-var init = false;
 var now;
 var amount = 5;
+var style = {
+  "display": "flex",
+  "justify-content": "space-between"
+}
 
 function hide() {
   document.getElementById("btn").style.display = "none";
@@ -12,30 +15,34 @@ function hide() {
   document.getElementById("clk").style.display = "none";
 }
 
-function clicked() {
-  if (init === false) { init = true; setTimeout(hide, amount * 1000); now = Date.now(); }
-}
-
 function App() {
   const [counter, setCount] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [clicked, setClicked] = useState(false);
+  const isFirstMount = useRef(true);
 
   useEffect(() => {
-    setInterval(() => {
-      setSeconds((Date.now() - now)/1000);
-    }, 10);
-  }, []);
+    if (!isFirstMount.current) {
+      now = Date.now();
+      console.log('helo');
+      setTimeout(hide, amount * 1000);
+      setInterval(() => {
+        setSeconds((Date.now() - now) / 1000);
+      }, 25);
+    }
+    else
+      isFirstMount.current = false;
+  }, [clicked]);
   return (
     <div className="App-header">
       <h1 className="App" id="clk"> Click! </h1>
-      <button id="btn" className = "btn" onClick={() => { setCount(counter + 1); clicked(); setSeconds((Date.now() - now) / 1000) }}>+</button>
-      <br></br>
-      <h2 id="secs" > {seconds} </h2>
+      <button id="btn" className="btn" onClick={() => { setCount(counter + 1); setClicked(true) }}>+</button>
+      <br />
+      
+      <h2 id="secs"> Timer: {seconds.toFixed(3)} </h2>
       <h2 id="cnt"> You clicked {counter} times</h2>
-      <h2 id="ans" hidden> {counter/amount} clicks per second </h2>
+      <h2 id="ans" hidden> {counter / amount} clicks per second </h2>
     </div>
   );
-
 }
-
 export default App;
